fdcf0e66b1d8d0062cb07f69587f8eb4d936acda
Vighnesh <113346403+Itachi-789@users.noreply.github.com>
2022-12-26 15:55:23 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import HigherOrderFunction from './components/hof';
+
+function App() {
+  return (
+    <div className="App">
+     <HigherOrderFunction/>
+    </div>
+  );
+}
+
+export default App;
